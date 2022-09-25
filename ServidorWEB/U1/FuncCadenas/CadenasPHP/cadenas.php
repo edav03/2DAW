519df5718c58a14f4d2dd58273429f149548d9f2
edav03 <edavalc03@hotmail.com>
2022-09-25 23:04:30 +0200

    Actualizacion Clases

---
+++ ServidorWEB/U1/FuncCadenas/CadenasPHP/cadenas.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Actividades cadena</title>
+    </head>
+    <body>
+        <h1>Actividades Cadenas</h1>
+        <h3>Hecho por Edison Alcocer</h3>
+
+        <a href="ej1.php">
+            Muestra el valor de un parámetro llamado nombre recibido por querystring eliminando los 
+            caracteres '/' del principio y el final si los hubiera (función trim). Si no se pasa un 
+            parámetro nombre se utilizará tu nombre de pila.
+        </a>
+    </body>
+</html>
---
+++ ServidorWEB/U1/FuncCadenas/CadenasPHP/ej1.php
@@ -0,0 +1,5 @@
+<?php
+
+$nombre = $_SERVER['QUERY_STRING'];
+
+echo $nombre;
