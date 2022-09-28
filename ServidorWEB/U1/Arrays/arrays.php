e7f1ff0256604d76eb7e7bb9595f02b4f664fb01
edav03 <edavalc03@hotmail.com>
2022-09-28 18:52:10 +0200

    Actualizacion Clases

--- ServidorWEB/U1/Arrays/arrays.php
+++ ServidorWEB/U1/Arrays/arrays.php
@@ -7,7 +7,8 @@ var_dump(count($FirstArray));
 echo "<h3>2. Crea una cadena que contenga los nombres de los alumnos existentes en el array separados por un espacio y muéstrala (función de strings implode) Muestra el array ordenado alfabéticamente (función asort)</h3>";
 
 echo "<br>";
-asort($FirstArray);
-print_r($FirstArray);
+// asort($FirstArray);
+$Second = asort($FirstArray);
+print_r($Second);
 
 echo "<h3>3. Muestra el array en el orden inverso al que se creó (función array_reverse) Muestra la posición que tiene tu nombre en el array (función array_search) Crea un array de alumnos donde cada elemento sea otro array que contenga el id, nombre y edad del alumno.</h3>";
