49e9e68923caf3c8a06eef88c83394e4d59a8fb9
edav03 <edavalc03@hotmail.com>
2022-10-03 19:17:47 +0200

    Actualizacion Clases

--- ClientWEB/U3/Act2/greetings.js
+++ ClientWEB/U3/Act2/greetings.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+var name, surname, salary, age;
+function SalaryFunction(){
+    this.name = document.getElementById(name).value;
+    this.surname = document.getElementById(surname).value;
+    this.salary = document.getElementById(salary).value;
+    this.age = document.getElementById(age).value;
+
+    document.write("<br>Nombre: " + this.name + "<br>Apellidos: " + this.surname + "<br>Salario: " + this.surname + "<br>Edad: " + this.surname);
+}
+
