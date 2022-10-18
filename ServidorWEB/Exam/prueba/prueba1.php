2f845ade5575aa5f15367472d409b1dd792d8e84
edav03 <edavalc03@hotmail.com>
2022-10-18 14:56:51 +0200

    Actualizacion Clases

---
+++ ServidorWEB/Exam/prueba/prueba1.php
@@ -0,0 +1,14 @@
+<?php
+
+$filas = array(
+    array(
+        'nombre' => 'Antonio', 'apellidos' => 'Gomez Gomez', 'telefono' => '675832145'),
+    array(
+        'nombre' =>'Pedro', 'apellidos' => 'Guillen Gaston', 'telefono' => '674562178'),
+    array(
+        'nombre' => 'Dolores', 'apellidos' => 'Candela Quema', 'telefono' => '689765432'),
+    array(
+        'nombre' =>'Ruben', 'apellidos' => 'Guardia Jurado', 'telefono' => '654213896')
+);
+
+while($row = )
