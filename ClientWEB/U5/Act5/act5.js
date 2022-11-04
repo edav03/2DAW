ef6ab2d92ccdd5c3d43c0a38f5745a30fb6818fe
edav03 <edavalc03@gmail.com>
2022-11-04 18:40:38 +0100

    Actualizacion Clases

--- ClientWEB/U5/Act5/act5.js
+++ ClientWEB/U5/Act5/act5.js
@@ -1,18 +1,33 @@
-var word = prompt("Write a word or leave it empty");
-var arrayWords = [];
-var i = 0;
+// var word = prompt("Write a word or leave it empty");
+// var arrayWords = [];
+// var i = 0;
 
-while (word != null && word != "") {
-    arrayWords[i] = word;
-    word = prompt("Write a word or leave it empty");
-    i++;
-}
+// while (word != null && word != "") {
+//     arrayWords[i] = word;
+//     word = prompt("Write a word or leave it empty");
+//     i++;
+// }
 
-recArray(arrayWords);
+// recArray(arrayWords);
 
-function recArray(array) {
-    const mapWords = array.map();
-    mapWords.forEach(element => {
-        document.write(element);
-    });
-}
+// function recArray(array) {
+//     const mapWords = array.map();
+//     mapWords.forEach(function(clave, valor){
+//         document.write(`Clave: ${clave}, Valor: ${valor} <br>`);
+//     });
+// }
+
+const arr = [
+    {key: 'name', value: 'Tom'},
+    {key: 'country', value: 'Chile'},
+  ];
+  
+//   const map1 = new Map(
+//     arr.map(object => {
+//       return [object.key, object.value];
+//     }),
+//   );
+  
+  // ️👇️ {'name' => 'Tom', 'country' => 'Chile'}
+  console.log(arr[0]);
+  
