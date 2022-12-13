8118991cc2e57614b66501e5aaff8ccf2e750d3a
edav03 <edavalc03@hotmail.com>
2022-12-13 11:59:12 +0100

    Actualizacion Clases

--- ServidorWEB/U4/ProyectosNode/Ejercicios/Sesion3/Ejercicio_3/index.js
+++ ServidorWEB/U4/ProyectosNode/Ejercicios/Sesion3/Ejercicio_3/index.js
@@ -1,4 +1,127 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/libros');
+mongoose.connect("mongodb://localhost:27017/libros");
+
+/* Esquema */
+let libroSchema = new mongoose.Schema({
+  titulo: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  editorial: {
+    type: String,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+});
+
+/* Aplicamos el esquema a modelo */
+let libro = mongoose.model("libro", libroSchema);
+
+/* Documentos */
+let libro1 = new libro({
+  titulo: "El capitan Alatriste",
+  editorial: "Alfaguara",
+  precio: 15,
+});
+
+let libro2 = new libro({
+  titulo: "El capitan Alatriste",
+  editorial: "Alfaguara",
+  precio: 15,
+});
+
+let libro3 = new libro({
+  titulo: "A",
+  precio: 12,
+});
+
+/* Añadimos los libros a la base de datos */
+libro1
+  .save()
+  .then((resultado) => {
+    console.log("Libro añadido: " + resultado);
+  })
+  .catch((error) => {
+    console.log("Error añadiendo contacto: " + error);
+  });
+
+libro2
+  .save()
+  .then((resultado) => {
+    console.log("Libro añadido: " + resultado);
+  })
+  .catch((error) => {
+    console.log("Error añadiendo contacto: " + error);
+  });
+
+libro3
+  .save()
+  .then((resultado) => {
+    console.log("Libro 3 guardado: " + resultado);
+  })
+  .catch((error) => {
+    console.log("Error añadiendo libro: " + error);
+  });
+
+/* Usamos metodo find y findId*/
+libro
+  .find({ edad: { $gte: 10, $lte: 20 } })
+  .then((resultado) => {
+    console.log("Resultado de la busqueda: " + resultado);
+  })
+  .catch((error) => {
+    console.log("Error " + error);
+  });
+
+libro
+  .findById("639845898e5d4dc641496877")
+  .then((resultado) => {
+    console.log("Resultado de la busqueda por ID: " + resultado);
+  })
+  .catch((error) => {
+    console.log("Error: " + error);
+  });
+
+/* Editar valor del precio de libro segun ID */
+libro
+  .findByIdAndUpdate(
+    "639845898e5d4dc641496876",
+    { $set: { precio: 200 } },
+    { new: true }
+  )
+  .then((resultado) => {
+    console.log("Libro Modificado: " + resultado);
+  })
+  .catch((error) => {
+    console.log("Error " + error);
+  });
+
+/* Eliminar libro segun ID */
+libro
+  .findByIdAndRemove("639845898e5d4dc641496877")
+  .then((resultado) => {
+    console.log("Libro eliminado " + resultado);
+  })
+  .catch((error) => {
+    console.log("Error: " + error);
+  });
+
+/* Editar valor del precio de libro segun ID y incrementar la version 1 unidad cada vez */
+libro
+  .findByIdAndUpdate(
+    "639845898e5d4dc641496876",
+    { $set: { precio: 250 }, $inc: { __v: 1 } },
+    { new: true }
+  )
+  .then((resultado) => {
+    console.log("Libro Modificado: " + resultado);
+  })
+  .catch((error) => {
+    console.log("Error " + error);
+  });
