91d4eba2dc6c6387020ba22543e783814b6cbde8
edav03 <edavalc03@hotmail.com>
2022-12-16 15:26:08 +0100

    Actualizacion Clases

---
+++ ClientWEB/U8/ejercicio3.js
@@ -0,0 +1,32 @@
+// Alto y ancho de ventana
+const width =
+  window.innerWidth ||
+  document.documentElement.clientWidth ||
+  document.body.clientWidth;
+
+const height =
+  window.innerHeight ||
+  document.documentElement.clientHeight ||
+  document.body.clientHeight;
+
+const capa = document.createElement("div");
+capa.style.position = "absolute";
+capa.style.border = "5px solid black";
+capa.style.height = height / 2 + "px";
+capa.style.width = width / 2 + "px";
+capa.style.left = "25%";
+capa.style.top = "25%";
+
+document.body.appendChild(capa);
+
+capa.addEventListener("mouseenter", () => {
+  capa.style.backgroundColor = "darkgreen";
+});
+
+capa.addEventListener("click", () => {
+  capa.style.backgroundColor = "red";
+});
+
+capa.addEventListener("contextmenu", () => {
+  capa.style.backgroundColor = "darkblue";
+});
