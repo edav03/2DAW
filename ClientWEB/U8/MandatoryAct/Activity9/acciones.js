10a0020320f00fb1d54331ef288c5268dd8ad2aa
edav03 <edavalc03@hotmail.com>
2022-12-21 15:19:24 +0100

    Actualizacion Clases

--- ClientWEB/U8/MandatoryAct/Activity9/acciones.js
+++ ClientWEB/U8/MandatoryAct/Activity9/acciones.js
@@ -5,17 +5,44 @@ let id = 0;
 
 button.onclick = (e) => {
   e.preventDefault();
-  if (tarea != "") {
-    console.log(tarea.value);
+  if (tarea.value != "") {
+    /* Li container the la lista */
+    let li = document.createElement("li");
+    li.setAttribute("id", id);
 
-    let newTarea = document.createElement("div");
-
+    /* Tarea introducida */
     let textTarea = document.createElement("p");
     textTarea.innerText = tarea.value;
+    textTarea.setAttribute("id", id);
 
-    lista.appendChild(newTarea);
-    newTarea.appendChild(textTarea);
+    /* Boton remove */
+    let btnTarea = document.createElement("button");
+    btnTarea.innerText = "REMOVE";
+    btnTarea.setAttribute("class", "remove");
+    btnTarea.setAttribute("id", id);
 
+    /* Se añade al HTML */
+    lista.appendChild(li);
+    li.append(textTarea, btnTarea);
+
+    /* Reset del input y añadimos al ID */
     tarea.value = "";
+    id++;
+
+    /* Poner tarea como primera */
+    textTarea.addEventListener("click", () => {
+      let thisID = textTarea.id;
+      let thisTAREA = document.getElementById(thisID);
+      thisTAREA.remove();
+      li.prepend(thisTAREA);
+      console.log("Has pulsado el texto");
+    });
+
+    /* Eliminar tarea */
+    btnTarea.onclick = () => {
+      let thisID = btnTarea.id;
+      let thisLI = document.getElementById(thisID);
+      thisLI.remove();
+    };
   }
 };
