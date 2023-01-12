adfab2704a1e6130ddb4befa2f5a8e82ad6ef703
edav03 <edavalc03@hotmail.com>
2023-01-12 12:30:08 +0100

    Actualizacion Clases

--- ServidorWEB/U4/ProyectosNode/Ejercicios/Sesion3/Ejercicio_3/index.js
+++ ServidorWEB/U4/ProyectosNode/Ejercicios/Sesion3/Ejercicio_3/index.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/libros");
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost:27017/libros')
 
 /* Esquema */
 let libroSchema = new mongoose.Schema({
@@ -18,110 +18,111 @@ let libroSchema = new mongoose.Schema({
     required: true,
     min: 0,
   },
-});
+})
 
 /* Aplicamos el esquema a modelo */
-let libro = mongoose.model("libro", libroSchema);
+let libro = mongoose.model('libro', libroSchema)
 
 /* Documentos */
 let libro1 = new libro({
-  titulo: "El capitan Alatriste",
-  editorial: "Alfaguara",
+  titulo: 'El capitan Alatriste',
+  editorial: 'Alfaguara',
   precio: 15,
-});
+})
 
 let libro2 = new libro({
-  titulo: "El capitan Alatriste",
-  editorial: "Alfaguara",
-  precio: 15,
-});
+  titulo: 'El nombre del viento',
+  editorial: 'Alfaguara',
+  precio: 25,
+})
 
 let libro3 = new libro({
-  titulo: "A",
+  titulo: 'La verdad sobre el caso Harry Quebert',
+  editorial: 'Santillana',
   precio: 12,
-});
+})
 
 /* Añadimos los libros a la base de datos */
 libro1
   .save()
   .then((resultado) => {
-    console.log("Libro añadido: " + resultado);
+    console.log('Libro 1 añadido: ' + resultado)
   })
   .catch((error) => {
-    console.log("Error añadiendo contacto: " + error);
-  });
+    console.log('Error añadiendo contacto: ' + error)
+  })
 
 libro2
   .save()
   .then((resultado) => {
-    console.log("Libro añadido: " + resultado);
+    console.log('Libro 2 añadido: ' + resultado)
   })
   .catch((error) => {
-    console.log("Error añadiendo contacto: " + error);
-  });
+    console.log('Error añadiendo contacto: ' + error)
+  })
 
 libro3
   .save()
   .then((resultado) => {
-    console.log("Libro 3 guardado: " + resultado);
+    console.log('Libro 3 añadido: ' + resultado)
   })
   .catch((error) => {
-    console.log("Error añadiendo libro: " + error);
-  });
+    console.log('Error añadiendo libro: ' + error)
+  })
 
 /* Usamos metodo find y findId*/
 libro
   .find({ edad: { $gte: 10, $lte: 20 } })
   .then((resultado) => {
-    console.log("Resultado de la busqueda: " + resultado);
+    console.log('Resultado de la busqueda: ' + resultado)
   })
   .catch((error) => {
-    console.log("Error " + error);
-  });
+    console.log('Error ' + error)
+  })
 
 libro
-  .findById("639845898e5d4dc641496877")
+  .findById('639845898e5d4dc641496877')
   .then((resultado) => {
-    console.log("Resultado de la busqueda por ID: " + resultado);
+    console.log('Resultado de la busqueda por ID: ' + resultado)
   })
   .catch((error) => {
-    console.log("Error: " + error);
-  });
+    console.log('Error: ' + error)
+  })
 
 /* Editar valor del precio de libro segun ID */
 libro
   .findByIdAndUpdate(
-    "639845898e5d4dc641496876",
+    '639845898e5d4dc641496876',
     { $set: { precio: 200 } },
     { new: true }
   )
   .then((resultado) => {
-    console.log("Libro Modificado: " + resultado);
+    console.log('Libro Modificado: ' + resultado)
   })
   .catch((error) => {
-    console.log("Error " + error);
-  });
+    console.log('Error ' + error)
+  })
 
 /* Eliminar libro segun ID */
 libro
-  .findByIdAndRemove("639845898e5d4dc641496877")
+  .findByIdAndRemove('639845898e5d4dc641496877')
   .then((resultado) => {
-    console.log("Libro eliminado " + resultado);
+    console.log('Libro eliminado ' + resultado)
   })
   .catch((error) => {
-    console.log("Error: " + error);
-  });
+    console.log('Error: ' + error)
+  })
 
 /* Editar valor del precio de libro segun ID y incrementar la version 1 unidad cada vez */
 libro
   .findByIdAndUpdate(
-    "639845898e5d4dc641496876",
+    '639845898e5d4dc641496876',
     { $set: { precio: 250 }, $inc: { __v: 1 } },
     { new: true }
   )
   .then((resultado) => {
-    console.log("Libro Modificado: " + resultado);
+    console.log('Libro Modificado: ' + resultado)
   })
   .catch((error) => {
-    console.log("Error " + error);
-  });
+    console.log('Error ' + error)
+  })
--- ServidorWEB/U4/ProyectosNode/Ejercicios/Sesion4/Ejercicio_4_2/index.js
+++ ServidorWEB/U4/ProyectosNode/Ejercicios/Sesion4/Ejercicio_4_2/index.js
@@ -1,4 +1,53 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost:27017/libros')
+
+let libroSchema = new mongoose.Schema({
+  titulo: {
+    type: String,
+    required: true,
+    minlength: 3,
+  },
+  editorial: {
+    type: String,
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+})
+
+let libro = mongoose.model('libro', libroSchema)
+
 let app = express()
+
+app.listen(8080)
+
+app.get('/libros', (req, res) => {
+  libro
+    .find()
+    .then((resultado) => {
+      res.send(resultado)
+    })
+    .catch((error) => {
+      res.send([])
+    })
+})
+
+app.get('/libros/:id', (req, res) => {
+  libro
+    .findById(req.params.id)
+    .then((resultado) => {
+      if (resultado) {
+        res.send({ error: false, resultado: resultado })
+      } else {
+        res.send({ error: true, mensajeError: 'No se ha encontrado resultado' })
+      }
+    })
+    .catch((error) => {
+      res.send([])
+    })
+})
