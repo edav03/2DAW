11c26b43ff1e3a3844ed8431aff8bef76e7f838f
edav03 <edavalc03@hotmail.com>
2023-01-20 00:08:54 +0100

    Actualizacion Clases

---
+++ ClientWEB/U10/Activities/Activity1/prueba.js
@@ -0,0 +1,65 @@
+async function getUser() {
+  let userInfo = await fetch('https://randomuser.me/api/?gender=female')
+    .then((response) => response.json())
+    .catch((error) => console.log('Error: ' + error))
+
+  // console.log(userInfo.results[0])
+  // return userInfo
+
+  /* Gets User's profile photo */
+  function getUserFoto() {
+    let photoUrl = userInfo
+
+    console.log('Photo URL: ' + photoUrl.results[0].picture.large)
+    return photoUrl.results[0].picture.large
+  }
+
+  /* Gets User's name */
+  function getUserName() {
+    let nameUrl = userInfo
+
+    console.log('Name: ' + nameUrl.results[0].name.first)
+    return nameUrl.results[0].name.first
+  }
+
+  /* Gets User's last name */
+  function getUserLastName() {
+    let lastNameUrl = userInfo
+
+    console.log('Last Name: ' + lastNameUrl.results[0].name.last)
+    return lastNameUrl.results[0].name.last
+  }
+
+  /* Gets User's email */
+  function getUserEmail() {
+    let emailUrl = userInfo
+
+    console.log('Email: ' + emailUrl.results[0].email)
+    return emailUrl.results[0].email
+  }
+
+  /* Gets User's direction */
+  function getUserDirection() {
+    let directionUrl = userInfo
+
+    console.log('Direction: ' + directionUrl.results[0].location.street.name)
+    return directionUrl.results[0].location.street.name
+  }
+
+  /* Gets User's state */
+  function getUserState() {
+    let stateUrl = userInfo
+
+    console.log('State: ' + stateUrl.results[0].location.state)
+    return stateUrl.results[0].location.state
+  }
+
+  getUserFoto()
+  getUserName()
+  getUserLastName()
+  getUserEmail()
+  getUserDirection()
+  getUserState()
+}
+
+getUser()
