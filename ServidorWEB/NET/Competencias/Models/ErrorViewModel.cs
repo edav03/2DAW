41b04f4bd475758d4a1adba5b343abff59406b26
edav03 <edavalc03@gmail.com>
2023-02-03 16:48:54 +0100

    Actualizacion Clases

---
+++ ServidorWEB/NET/Competencias/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Competencias.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
