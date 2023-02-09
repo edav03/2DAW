1dbee5757e1f362e89c45d11366926bcecc97e81
edav03 <edavalc03@gmail.com>
2023-02-09 16:31:22 +0100

    Actualizacion Clases

--- ServidorWEB/NET/Competencias/Controllers/HomeController.cs
+++ ServidorWEB/NET/Competencias/Controllers/HomeController.cs
@@ -16,8 +16,8 @@ namespace Competencias.Controllers
 
         public IActionResult Index()
         {
-            Carrera cr = new Carrera();
-            return View(cr.GetAll());
+            MantenimientoCompetencias cr = new MantenimientoCompetencias();
+            return View(cr.GetAllParticipantes());
         }
 
         public IActionResult Alta()
@@ -29,7 +29,7 @@ namespace Competencias.Controllers
         public IActionResult Alta(IFormCollection collection)
         {
 
-            Carrera car = new Carrera();
+            MantenimientoCompetencias car = new MantenimientoCompetencias();
             Participantes part = new Participantes();
 
             part.IdParticipantes = int.Parse(collection["IdParticipantes"].ToString());
@@ -44,9 +44,32 @@ namespace Competencias.Controllers
             return RedirectToAction("Index");
         }
 
+        public IActionResult AltaCarrera() 
+        { 
+            return View(); 
+        }
+
+        [HttpPost]
+        public IActionResult AltaCarrera(IFormCollection collection)
+        {
+
+            MantenimientoCompetencias car = new MantenimientoCompetencias();
+            Carrera race = new Carrera();
+
+            race.IdCarrera = int.Parse(collection["IdCarrera"].ToString());
+            race.NumCarrera = int.Parse(collection["NumCarrera"].ToString());
+            race.DescripcionCarrera = collection["DescripcionCarrera"].ToString();
+            race.FechaCarrera = collection["FechaCarrera"].ToString();
+            race.Distanciaenmetros = collection["Distanciaenmetros"].ToString();
+            race.HoraInicio = collection["HoraInicio"].ToString();
+
+            car.AltaCarrera(race);
+            return RedirectToAction("Carreras");
+        }
+
         public IActionResult Editar(int IdParticipantes)
         {
-            Carrera car = new Carrera();
+            MantenimientoCompetencias car = new MantenimientoCompetencias();
             Participantes part = car.GetId(IdParticipantes);
             return View(part);
         }
@@ -54,28 +77,73 @@ namespace Competencias.Controllers
         [HttpPost]
         public IActionResult Editar(IFormCollection collection)
         {
-            Carrera car = new Carrera();
+            int IdParticipantes = int.Parse(collection["IdParticipantes"].ToString());
+            int NumCarrera = int.Parse(collection["NumCarrera"].ToString());
+            int Dorsal = int.Parse(collection["Dorsal"].ToString());
+            string Apellidos = collection["Apellidos"].ToString();
+            string Nombre = collection["Nombre"].ToString();
+            string Dni = collection["Dni"].ToString();
+            string Club = collection["Club"].ToString();
+
             Participantes part = new Participantes();
-
-            part.IdParticipantes = int.Parse(collection["IdParticipantes"].ToString());
-            part.NumCarrera = int.Parse(collection["NumCarrera"].ToString());
-            part.Dorsal = int.Parse(collection["Dorsal"].ToString());
-            part.Apellidos = collection["Apellidos"].ToString();
-            part.Nombre = collection["Nombre"].ToString();
-            part.Dni = collection["Dni"].ToString();
-            part.Club = collection["Club"].ToString();
-
-            car.Modificar(part); ;
+            part.IdParticipantes = IdParticipantes;
+            part.NumCarrera = NumCarrera;
+            part.Dorsal = Dorsal;
+            part.Apellidos = Apellidos;
+            part.Nombre = Nombre;
+            part.Dni = Dni;
+            part.Club = Club;
+           
+            MantenimientoCompetencias car = new MantenimientoCompetencias();
+            car.Modificar(part);
 
             return RedirectToAction("Index");
         }
 
-        public IActionResult Delete(int IdParticipantes)
+        public IActionResult EditarCarrera(int cod) 
         {
-            Carrera car = new Carrera();
+            MantenimientoCompetencias car = new MantenimientoCompetencias();
+            Carrera race = car.GetIdCarrera(cod);
+
+            return View(race); 
+        }
+
+        [HttpPost]
+        public IActionResult EditarCarrera(IFormCollection collection)
+        {
+            int IdCarrera = int.Parse(collection["IdCarrera"].ToString());
+            string DescripcionCarrera = collection["DescripcionCarrera"].ToString();
+            string FechaCarrera = collection["FechaCarrera"].ToString();
+            string Distanciaenmetros = collection["Distanciaenmetros"].ToString();
+            string HoraInicio = collection["HoraInicio"].ToString();
+
+            Carrera race = new Carrera();
+            race.IdCarrera = IdCarrera;
+            race.DescripcionCarrera = DescripcionCarrera;
+            race.FechaCarrera = FechaCarrera;
+            race.Distanciaenmetros = Distanciaenmetros;
+            race.HoraInicio = HoraInicio;
+
+            MantenimientoCompetencias car = new MantenimientoCompetencias();
+            car.ModificarCarrera(race);
+
+            return RedirectToAction("Carreras");
+        }
+
+            public IActionResult Delete(int IdParticipantes)
+        {
+            MantenimientoCompetencias car = new MantenimientoCompetencias();
             car.Borrar(IdParticipantes);
 
             return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCarrera(int cod)
+        {
+            MantenimientoCompetencias car = new MantenimientoCompetencias();
+            car.BorrarCarrera(cod);
+
+            return RedirectToAction("Carreras");
         }
 
         public IActionResult Importar()
@@ -83,12 +151,13 @@ namespace Competencias.Controllers
             return View();
         }
 
-        public IActionResult Participantes()
+        public IActionResult Carreras()
         {
-            return View();
+            MantenimientoCompetencias cr = new MantenimientoCompetencias();
+            return View(cr.GetAllCarrera());
         }
 
-        public IActionResult Privacy()
+        public IActionResult CodigoChips()
         {
             return View();
         }
---
+++ ServidorWEB/NET/Competencias/Models/MantenimientoCompetencias.cs
@@ -0,0 +1,272 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Competencias.Models
+{
+    public class MantenimientoCompetencias
+    {
+        private SqlConnection con;
+        private void Conectar()
+        {
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+           .AddJsonFile("appSettings.json", optional: true, reloadOnChange: true);
+            IConfiguration _configuration = builder.Build();
+            string constr = _configuration.GetConnectionString("DefaultConnection");
+            //Console.WriteLine(myConnectionString1);
+
+            con = new SqlConnection(constr);
+        }
+
+        public int Alta(Participantes part)
+        {
+
+            Conectar();
+            SqlCommand cmd = new SqlCommand(
+                        "insert into participantes (IdParticipantes, NumCarrera, Dorsal, Apellidos, Nombre, Dni, Club) " +
+                        "values(@IdParticipantes,@NumCarrera,@Dorsal,@Apellidos,@Nombre,@Dni,@Club)", con);
+
+            cmd.Parameters.Add("@IdParticipantes", System.Data.SqlDbType.Int);
+            cmd.Parameters.Add("@NumCarrera", System.Data.SqlDbType.Int);
+            cmd.Parameters.Add("@Dorsal", System.Data.SqlDbType.Int);
+            cmd.Parameters.Add("@Apellidos", System.Data.SqlDbType.VarChar);
+            cmd.Parameters.Add("@Nombre", System.Data.SqlDbType.VarChar);
+            cmd.Parameters.Add("@Dni", System.Data.SqlDbType.VarChar);
+            cmd.Parameters.Add("@Club", System.Data.SqlDbType.VarChar);
+
+            cmd.Parameters["@IdParticipantes"].Value = part.IdParticipantes.ToString();
+            cmd.Parameters["@NumCarrera"].Value = part.NumCarrera.ToString();
+            cmd.Parameters["@Dorsal"].Value = part.Dorsal.ToString();
+            cmd.Parameters["@Apellidos"].Value = part.Apellidos.ToString();
+            cmd.Parameters["@Nombre"].Value = part.Nombre.ToString();
+            cmd.Parameters["@Dni"].Value = part.Dni.ToString();
+            cmd.Parameters["@Club"].Value = part.Club.ToString();
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+
+        public int AltaCarrera(Carrera race)
+        {
+
+            Conectar();
+            SqlCommand cmd = new SqlCommand(
+                        "insert into carreras (IdCarrera, NumCarrera, DescripcionCarrera, FechaCarrera, Distanciaenmetros, HoraInicio) " +
+                        "values(@IdCarrera,@NumCarrera,@DescripcionCarrera,@FechaCarrera,@Distanciaenmetros, @HoraInicio)", con);
+
+            cmd.Parameters.Add("@IdCarrera", System.Data.SqlDbType.Int);
+            cmd.Parameters.Add("@NumCarrera", System.Data.SqlDbType.Int);
+            cmd.Parameters.Add("@DescripcionCarrera", System.Data.SqlDbType.VarChar);
+            cmd.Parameters.Add("@FechaCarrera", System.Data.SqlDbType.VarChar);
+            cmd.Parameters.Add("@Distanciaenmetros", System.Data.SqlDbType.VarChar);
+            cmd.Parameters.Add("@HoraInicio", System.Data.SqlDbType.VarChar);
+
+            cmd.Parameters["@IdCarrera"].Value = race.IdCarrera.ToString();
+            cmd.Parameters["@NumCarrera"].Value = race.NumCarrera.ToString();
+            cmd.Parameters["@DescripcionCarrera"].Value = race.DescripcionCarrera.ToString();
+            cmd.Parameters["@FechaCarrera"].Value = race.FechaCarrera.ToString();
+            cmd.Parameters["@Distanciaenmetros"].Value = race.Distanciaenmetros.ToString();
+            cmd.Parameters["@HoraInicio"].Value = race.HoraInicio.ToString();
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+
+        public List<Participantes> GetAllParticipantes()
+        {
+            Conectar();
+            List<Participantes> Allparticipantes = new List<Participantes>();
+            string sql = "select * from participantes";
+            SqlCommand comando = new SqlCommand(sql, con);
+            con.Open();
+            SqlDataReader registros = comando.ExecuteReader();
+            while (registros.Read())
+            {
+
+                Participantes part = new Participantes();
+                part.IdParticipantes = int.Parse(registros["IdParticipantes"].ToString());
+                part.Nombre = registros["Nombre"].ToString();
+                part.Apellidos = registros["Apellidos"].ToString();
+                part.Dorsal = int.Parse(registros["Dorsal"].ToString());
+                part.Club = registros["Club"].ToString();
+
+
+                Allparticipantes.Add(part);
+            }
+            return Allparticipantes;
+        }
+
+        public Participantes GetId(int IdParticipantes)
+        {
+            Conectar();
+            string sql = "select IdParticipantes, NumCarrera, Dorsal, Apellidos, Nombre, Dni, Club from participantes where IdParticipantes=@IdParticipantes";
+            SqlCommand comando = new SqlCommand(sql, con);
+            comando.Parameters.Add("@IdParticipantes", SqlDbType.Int);
+            comando.Parameters["@IdParticipantes"].Value = IdParticipantes;
+            con.Open();
+            SqlDataReader registros = comando.ExecuteReader();
+            Participantes part = new Participantes();
+            if (registros.Read())
+            {
+                part.IdParticipantes = int.Parse(registros["IdParticipantes"].ToString());
+                part.NumCarrera = int.Parse(registros["NumCarrera"].ToString());
+                part.Dorsal = int.Parse(registros["Dorsal"].ToString());
+                part.Apellidos = registros["Apellidos"].ToString();
+                part.Nombre = registros["Nombre"].ToString();
+                part.Dni = registros["Dni"].ToString();
+                part.Club = registros["Club"].ToString();
+            }
+            con.Close();
+            return part;
+        }
+
+        public Carrera GetIdCarrera(int cod)
+        {
+            Conectar();
+            string sql = "select * from carreras where IdCarrera=@cod";
+            SqlCommand comando = new SqlCommand(sql, con);
+            comando.Parameters.Add("@cod", SqlDbType.Int);
+            comando.Parameters["@cod"].Value = cod;
+            con.Open();
+            SqlDataReader registros = comando.ExecuteReader();
+            Carrera race = new Carrera();
+            if (registros.Read())
+            {
+                race.IdCarrera = int.Parse(registros["IdCarrera"].ToString());
+                race.DescripcionCarrera = registros["DescripcionCarrera"].ToString();
+                race.FechaCarrera = registros["FechaCarrera"].ToString();
+                race.Distanciaenmetros = registros["Distanciaenmetros"].ToString();
+                race.HoraInicio = registros["HoraInicio"].ToString();
+            }
+            con.Close();
+            return race;
+        }
+
+        public int Modificar(Participantes part)
+        {
+            Conectar();
+            string sql = @"update participantes set 
+                            NumCarrera=@NumCarrera,
+                            Dorsal=@Dorsal,
+                            Apellidos=@Apellidos,
+                            Nombre=@Nombre,
+                            Dni=@Dni,
+                            Club=@Club
+            where IdParticipantes=@IdParticipantes";
+
+            SqlCommand comando = new SqlCommand(sql, con);
+            comando.Parameters.Add("@IdParticipantes", SqlDbType.Int);
+            comando.Parameters.Add("@NumCarrera", SqlDbType.Int);
+            comando.Parameters.Add("@Dorsal", SqlDbType.Int);
+            comando.Parameters.Add("@Apellidos", SqlDbType.VarChar);
+            comando.Parameters.Add("@Nombre", SqlDbType.VarChar);
+            comando.Parameters.Add("@Dni", SqlDbType.VarChar);
+            comando.Parameters.Add("@Club", SqlDbType.VarChar);
+            comando.Parameters["@IdParticipantes"].Value = part.IdParticipantes;
+            comando.Parameters["@NumCarrera"].Value = part.NumCarrera;
+            comando.Parameters["@Dorsal"].Value = part.Dorsal;
+            comando.Parameters["@Apellidos"].Value = part.Apellidos;
+            comando.Parameters["@Nombre"].Value = part.Nombre;
+            comando.Parameters["@Dni"].Value = part.Dni;
+            comando.Parameters["@Club"].Value = part.Club;
+
+            con.Open();
+            int i = comando.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+
+        public int ModificarCarrera(Carrera race)
+        {
+            Conectar();
+            string sql = @"update carreras set 
+                            IdCarrera=@IdCarrera,
+                            DescripcionCarrera=@DescripcionCarrera,
+                            FechaCarrera=@FechaCarrera,
+                            Distanciaenmetros=@Distanciaenmetros,
+                            HoraInicio=@HoraInicio
+            where IdCarrera=@IdCarrera";
+
+            SqlCommand comando = new SqlCommand(sql, con);
+            comando.Parameters.Add("@IdCarrera", SqlDbType.Int);
+            comando.Parameters.Add("@DescripcionCarrera", SqlDbType.VarChar);
+            comando.Parameters.Add("@FechaCarrera", SqlDbType.VarChar);
+            comando.Parameters.Add("@Distanciaenmetros", SqlDbType.VarChar);
+            comando.Parameters.Add("@HoraInicio", SqlDbType.VarChar);
+            comando.Parameters["@IdCarrera"].Value = race.IdCarrera;
+            comando.Parameters["@DescripcionCarrera"].Value = race.DescripcionCarrera;
+            comando.Parameters["@FechaCarrera"].Value = race.FechaCarrera;
+            comando.Parameters["@Distanciaenmetros"].Value = race.Distanciaenmetros;
+            comando.Parameters["@HoraInicio"].Value = race.HoraInicio;
+
+            con.Open();
+            int i = comando.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+
+            public int Borrar(int IdParticipantes)
+        {
+
+            Conectar();
+
+            string sql = "delete from participantes where IdParticipantes=@IdParticipantes";
+
+            SqlCommand sentencia = new SqlCommand(sql, con);
+
+            sentencia.Parameters.Add("@IdParticipantes", SqlDbType.Int);
+            sentencia.Parameters["@IdParticipantes"].Value = IdParticipantes;
+
+            con.Open();
+            int i = sentencia.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+
+        public int BorrarCarrera(int cod)
+        {
+            Conectar();
+            string sql = "delete from carreras where IdCarrera=@cod";
+            SqlCommand sentencia = new SqlCommand(sql, con);
+
+            sentencia.Parameters.Add("@cod", SqlDbType.Int);
+            sentencia.Parameters["@cod"].Value = cod;
+
+            con.Open();
+            int i = sentencia.ExecuteNonQuery();
+            con.Close();
+            return i;
+        }
+
+        public List<Carrera> GetAllCarrera()
+        {
+            Conectar();
+            List<Carrera> Allcarrera = new List<Carrera>();
+            string sql = "select * from carreras";
+            SqlCommand comando = new SqlCommand(sql, con);
+            con.Open();
+            SqlDataReader registros = comando.ExecuteReader();
+            while (registros.Read())
+            {
+
+                Carrera race = new Carrera();
+                race.IdCarrera = int.Parse(registros["IdCarrera"].ToString());
+                race.DescripcionCarrera = registros["DescripcionCarrera"].ToString();
+                race.FechaCarrera = registros["FechaCarrera"].ToString();
+                race.Distanciaenmetros = registros["Distanciaenmetros"].ToString();
+                race.HoraInicio = registros["HoraInicio"].ToString();
+
+
+                Allcarrera.Add(race);
+            }
+            return Allcarrera;
+        }
+
+    }
+
+}
